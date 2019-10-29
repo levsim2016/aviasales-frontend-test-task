5fc9667e9464e10ce329c2bc941c72bd7da58aca
Lev Simionenko <levsim2016@gmail.com>
2019-10-29 22:50:27 +0300

    optimize checkbox check handling

--- src/ui-elements/components/checkbox/Checkbox.tsx
+++ src/ui-elements/components/checkbox/Checkbox.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useCallback, MouseEvent } from 'react';
 import styles from './checkbox.module.scss';
 import CheckboxMarker from '../checkbox-marker/CheckboxMarker';
 import CheckboxProps from 'ui-elements/interfaces/CheckboxProps';
 
 const Checkbox: React.FC<CheckboxProps> = (props: CheckboxProps) => {
-  const { label, checked } = props;
+  const { label, checked, checkHandler } = props;
+
+  const clickHandler = useCallback((event: MouseEvent) => {
+    checkHandler(!checked);
+  }, [checked, checkHandler]);
+
   return (
-    <div className={styles.layout}>
+    <div className={styles.layout} onClick={clickHandler}>
       <CheckboxMarker checked={checked}></CheckboxMarker>
       <span>
         {label}
