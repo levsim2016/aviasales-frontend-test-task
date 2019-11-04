d5ca48443562fc969755ac9bb2cba699f6c27476
Lev Simionenko <levsim2016@gmail.com>
2019-11-05 01:20:39 +0300

    add Open Sans fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+
 import App from './app/components/app/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
