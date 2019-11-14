0c6f2d2bc2e2cb6da536396114cc229a70875a35
Lev Simionenko <levsim2016@gmail.com>
2019-11-14 03:14:05 +0300

    remove mocked data from ticket component

--- src/app/components/ticket/Ticket.tsx
+++ src/app/components/ticket/Ticket.tsx
@@ -5,6 +5,7 @@ import Card from 'ui-elements/components/card/Card';
 import RouteDetails from '../route-details/RouteDetails';
 import { ITicketProps } from 'app/interfaces/ITicketProps';
 import { CARRIERS_LOGO_CDN_URL } from 'app/constants/carrierLogoCdnUrl';
+import { getLocalizedPrice } from 'shared/getLocalizedPrice';
 
 function getCarrierLogoUrl(carrierIataCode: string): string {
   return CARRIERS_LOGO_CDN_URL + carrierIataCode + '.png';
@@ -12,33 +13,31 @@ function getCarrierLogoUrl(carrierIataCode: string): string {
 
 export const Ticket: React.FC<ITicketProps> = (props: ITicketProps) => {
   const { ticketCard, header, priceLabel, carrierLogoWrapper } = styles;
-  const { price, carrier } = props;
+  const { price, carrier, segments } = props;
 
   const ticketClassesString = [ticketCard, props.className].join(' ');
+  const carrierLogoUrl = getCarrierLogoUrl(carrier);
+  const localizedPrice = getLocalizedPrice(price);
 
   return (
     <Card className={ticketClassesString}>
       <div className={header}>
-        <h3 className={priceLabel}>{price}</h3>
+        <h3 className={priceLabel}>{localizedPrice}</h3>
         <div className={carrierLogoWrapper}>
-          <img src={getCarrierLogoUrl(carrier)} alt='Logo of airline' />
+          <img src={carrierLogoUrl} alt='Logo of airline' />
         </div>
       </div>
       <ul className={styles.routes}>
-        <RouteDetails
-          origin='MOW'
-          destination='HKT'
-          date='2121421412'
-          duration={435}
-          stops={['HKG', 'JNB']}
-        ></RouteDetails>
-        <RouteDetails
-          origin='HKT'
-          destination='MOW'
-          date='2121421412'
-          duration={435}
-          stops={['HKG', 'JNB']}
-        ></RouteDetails>
+        {segments.map((routeSegment, i) => (
+          <RouteDetails
+            origin={routeSegment.origin}
+            destination={routeSegment.destination}
+            date={routeSegment.date}
+            duration={routeSegment.duration}
+            stops={routeSegment.stops}
+            key={i}
+          ></RouteDetails>
+        ))}
       </ul>
     </Card>
   );
