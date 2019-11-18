ea294059bc594f2e38138da4a96bd8ccbc0826cd
Lev Simionenko <levsim2016@gmail.com>
2019-11-19 01:53:35 +0300

    add case for fetch searchId action into reducer

--- src/app/interfaces/IAppState.ts
+++ src/app/interfaces/IAppState.ts
@@ -1,5 +1,7 @@
 import { ITicket } from './ITicket';
+import { ISearchIdResponse } from './ISearchIdResponse';
 
 export interface IAppState {
   tickets: ITicket[];
+  searchId: ISearchIdResponse['searchId'];
 }
--- src/app/reducers/app.reducer.ts
+++ src/app/reducers/app.reducer.ts
@@ -2,53 +2,17 @@ import { AnyAction } from 'redux';
 
 import { IAppState } from 'app/interfaces/IAppState';
 import { ISetTicketsAction, SET_TICKETS } from 'app/actions/SetTicketsAction';
+import {
+  ISetSearchIdAction,
+  SET_SEARCH_ID,
+} from 'app/actions/SetSearchIdAction';
 
 const initialState: IAppState = {
-  tickets: [
-    {
-      price: 43667,
-      carrier: 'S7',
-      segments: [
-        {
-          origin: 'MOW',
-          destination: 'HKT',
-          date: new Date('2019-11-22T15:27:00.000Z'),
-          stops: ['DXB', 'KUL', 'AUH'],
-          duration: 1891,
-        },
-        {
-          origin: 'MOW',
-          destination: 'HKT',
-          date: new Date('2019-12-12T04:21:00.000Z'),
-          stops: [],
-          duration: 1494,
-        },
-      ],
-    },
-    {
-      price: 67002,
-      carrier: 'EK',
-      segments: [
-        {
-          origin: 'MOW',
-          destination: 'HKT',
-          date: new Date('2019-11-21T21:18:00.000Z'),
-          stops: [],
-          duration: 666,
-        },
-        {
-          origin: 'MOW',
-          destination: 'HKT',
-          date: new Date('2019-12-12T07:10:00.000Z'),
-          stops: ['AUH'],
-          duration: 1734,
-        },
-      ],
-    },
-  ],
+  tickets: [],
+  searchId: '',
 };
 
-type ActionsUnion = ISetTicketsAction;
+type ActionsUnion = ISetTicketsAction | ISetSearchIdAction;
 
 export function appReducer(
   state: IAppState = initialState,
@@ -60,6 +24,12 @@ export function appReducer(
         ...state,
         tickets: action.tickets,
       };
+
+    case SET_SEARCH_ID:
+      return {
+        ...state,
+      };
+
     default:
       return state;
   }
