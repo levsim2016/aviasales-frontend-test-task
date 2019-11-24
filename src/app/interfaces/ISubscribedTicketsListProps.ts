af0c8603b09acfda89d0c86e359a58ad63300c64
Lev Simionenko <levsim2016@gmail.com>
2019-11-25 02:05:05 +0300

    make lazy loading for tickets list

--- src/app/interfaces/ISubscribedTicketsListProps.ts
+++ src/app/interfaces/ISubscribedTicketsListProps.ts
@@ -5,6 +5,8 @@ import { ISetSearchIdAction } from 'app/actions/SetSearchIdAction';
 
 export interface ISubscribedTicketsListProps {
     tickets: ITicket[];
+    loadingStatus: boolean;
     fetchTickets: () => Promise<ISetTicketsAction>;
+    updateTickets: () => Promise<ISetTicketsAction>;
     getSearchId: () => Promise<ISetSearchIdAction>;
 }
